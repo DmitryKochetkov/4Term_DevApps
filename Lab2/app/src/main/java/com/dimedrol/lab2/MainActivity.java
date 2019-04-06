b0b0b53409ce6e6f22e817b46b3b49d5dc7409da
DmitryKochetkov <34890452+DmitryKochetkov@users.noreply.github.com>
2019-04-06 23:10:11 +0300

    Lab 2: JSON (Volley)

---
+++ Lab2/app/src/main/java/com/dimedrol/lab2/MainActivity.java
@@ -0,0 +1,24 @@
+package com.dimedrol.lab2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import org.json.JSONArray;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        RecyclerView recyclerView = findViewById(R.id.recycler);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setHasFixedSize(true);
+        adapter = new MyAdapter();
+        recyclerView.setAdapter(adapter);
+    }
+}
--- Lab2/app/src/main/java/com/dimedrol/lab2/SplashScreenActivity.java
+++ Lab2/app/src/main/java/com/dimedrol/lab2/SplashScreenActivity.java
@@ -1,13 +1,57 @@
 package com.dimedrol.lab2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 
 public class SplashScreenActivity extends AppCompatActivity {
+
+    JSONArray a;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        String url = "https://raw.githubusercontent.com/wesleywerner/ancienttech/02decf875616dd9692b31658d92e64a20d99f816/src/data/techs.ruleset.json";
+        String url_img = "https://raw.githubusercontent.com/wesleywerner/ancienttech/02decf875616dd9692b31658d92e64a20d99f816/src/images/tech/";
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    JSONArray jsonArray = response.getJSONArray("");
+                    for (int i = 1; i < jsonArray.length(); i++) { //i = 1?
+                        JSONObject tech = jsonArray.getJSONObject(i);
+                        String graphic = tech.getString("graphic");
+                        String name = tech.getString("name");
+                        //String helptext = tech.getString("helptext");
+                    }
+                    a = jsonArray;
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        });
+        RecyclerView r = findViewById(R.id.recycler);
+
+        Intent intent = new Intent(this, MainActivity.class);
+
+        startActivity(intent);
     }
 }
