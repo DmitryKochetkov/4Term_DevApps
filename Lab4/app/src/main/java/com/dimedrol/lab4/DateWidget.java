dc63de25acd3c9041dd6c91c167f6728a84e28cf
DmitryKochetkov <34890452+DmitryKochetkov@users.noreply.github.com>
2019-05-31 23:15:49 +0300

    Lab4: notification

--- Lab4/app/src/main/java/com/dimedrol/lab4/DateWidget.java
+++ Lab4/app/src/main/java/com/dimedrol/lab4/DateWidget.java
@@ -1,8 +1,11 @@
 package com.dimedrol.lab4;
 
+import android.app.Notification;
+import android.app.NotificationManager;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.support.v4.app.NotificationCompat;
 import android.view.LayoutInflater;
 import android.widget.RemoteViews;
 import android.widget.TextView;
@@ -14,12 +17,15 @@ import java.util.Date;
 import java.util.GregorianCalendar;
 import java.util.Locale;
 
+import static android.content.Context.NOTIFICATION_SERVICE;
+
 /**
  * Implementation of App Widget functionality.
  */
 public class DateWidget extends AppWidgetProvider {
 
     static Calendar aim;
+    static boolean done = false;
 
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
                                 int appWidgetId) {
@@ -29,8 +35,9 @@ public class DateWidget extends AppWidgetProvider {
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.date_widget);
 
         DateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy", Locale.getDefault());
-        //views.setTextViewText(R.id.date_text, dateFormat.format(aim.getTime()));
-        views.setTextViewText(R.id.date_text, aim.getTime().toString());
+        Date d = aim.getTime();
+        views.setTextViewText(R.id.date_text, "Notification date: " + dateFormat.format(d));
+        //views.setTextViewText(R.id.date_text, "Notification date: " + dateFormat.format(aim.getTime()));
 
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
@@ -41,6 +48,24 @@ public class DateWidget extends AppWidgetProvider {
         // There may be multiple widgets active, so update all of them
         for (int appWidgetId : appWidgetIds) {
             updateAppWidget(context, appWidgetManager, appWidgetId);
+        }
+        Calendar now = new GregorianCalendar();
+        now.add(Calendar.DAY_OF_YEAR, 1); //убрать
+        DateFormat day = new SimpleDateFormat("dd", Locale.getDefault());
+        DateFormat hour = new SimpleDateFormat("hh", Locale.getDefault());
+        if (hour.format(now.getTime()) == "10" && !done)
+        {
+            done = true;
+            NotificationCompat.Builder builder =
+                    new NotificationCompat.Builder(context)
+                            .setSmallIcon(R.mipmap.ic_launcher)
+                            .setContentTitle("Date Notification")
+                            .setContentText("It's 9 o'clock!");
+
+            Notification notification = builder.build();
+
+            NotificationManager notificationManager = (NotificationManager) context.getSystemService(NOTIFICATION_SERVICE);
+            notificationManager.notify(1, notification);
         }
     }
 
